5fa5fe588cfdc132ba6625b76619ccc9b76a2606
Akbar Ahmadi <hero.lionel@gmail.com>
2018-12-20 14:24:27 +0700

    shift key working

--- Final Project/Final Project/Tool/EditTextTool.cs
+++ Final Project/Final Project/Tool/EditTextTool.cs
@@ -84,23 +84,11 @@ namespace Final_Project.Tool
             }
             else if (char.IsLetterOrDigit((char)e.KeyCode))
             {
-                Char insertedKey;
+                Char insertedKey = KeyToChar(e.KeyCode, e.Shift);
                 Debug.WriteLine(e.KeyCode.ToString() + " pressed");
-                if (char.IsDigit((char)e.KeyCode))
-                {
-                     insertedKey = (char)(int)e.KeyCode;
-                }
-                else if (!Control.IsKeyLocked(Keys.CapsLock))
-                {
-                    insertedKey = e.KeyCode.ToString().ToLower()[0];
-                }
-                else
-                {
-                    insertedKey = e.KeyCode.ToString()[0];
-                }
                 this.selectedText.SetText(currentText + insertedKey);
-                canvas.Repaint();
             }
+            canvas.Repaint();
         }
 
         public void ToolKeyPressDown(object sender, KeyPressEventArgs e)
@@ -118,6 +106,83 @@ namespace Final_Project.Tool
         public void ToolKeyUp(object sender, KeyEventArgs e)
         {
 
+        }
+
+        public Char KeyToChar(Keys key, bool shift)
+        {
+            bool capslock = Control.IsKeyLocked(Keys.CapsLock);
+            bool iscap = (capslock && !shift) || (!capslock && shift);
+            switch (key)
+            {
+                case Keys.Enter: return '\n';
+                case Keys.A: return (iscap ? 'A' : 'a');
+                case Keys.B: return (iscap ? 'B' : 'b');
+                case Keys.C: return (iscap ? 'C' : 'c');
+                case Keys.D: return (iscap ? 'D' : 'd');
+                case Keys.E: return (iscap ? 'E' : 'e');
+                case Keys.F: return (iscap ? 'F' : 'f');
+                case Keys.G: return (iscap ? 'G' : 'g');
+                case Keys.H: return (iscap ? 'H' : 'h');
+                case Keys.I: return (iscap ? 'I' : 'i');
+                case Keys.J: return (iscap ? 'J' : 'j');
+                case Keys.K: return (iscap ? 'K' : 'k');
+                case Keys.L: return (iscap ? 'L' : 'l');
+                case Keys.M: return (iscap ? 'M' : 'm');
+                case Keys.N: return (iscap ? 'N' : 'n');
+                case Keys.O: return (iscap ? 'O' : 'o');
+                case Keys.P: return (iscap ? 'P' : 'p');
+                case Keys.Q: return (iscap ? 'Q' : 'q');
+                case Keys.R: return (iscap ? 'R' : 'r');
+                case Keys.S: return (iscap ? 'S' : 's');
+                case Keys.T: return (iscap ? 'T' : 't');
+                case Keys.U: return (iscap ? 'U' : 'u');
+                case Keys.V: return (iscap ? 'V' : 'v');
+                case Keys.W: return (iscap ? 'W' : 'w');
+                case Keys.X: return (iscap ? 'X' : 'x');
+                case Keys.Y: return (iscap ? 'Y' : 'y');
+                case Keys.Z: return (iscap ? 'Z' : 'z');
+                case Keys.D0: return (shift ? ')' : '0');
+                case Keys.D1: return (shift ? '!' : '1');
+                case Keys.D2: return (shift ? '@' : '2');
+                case Keys.D3: return (shift ? '#' : '3');
+                case Keys.D4: return (shift ? '$' : '4');
+                case Keys.D5: return (shift ? '%' : '5');
+                case Keys.D6: return (shift ? '^' : '6');
+                case Keys.D7: return (shift ? '&' : '7');
+                case Keys.D8: return (shift ? '*' : '8');
+                case Keys.D9: return (shift ? '(' : '9');
+                case Keys.Add: return (shift ? '+' : '=');
+                case Keys.OemMinus: return (shift ? '_' : '-');
+                case Keys.OemQuestion: return (shift ? '?' : '/');
+                case Keys.Oemcomma: return (shift ? '<' : ',');
+                case Keys.OemPeriod: return (shift ? '>' : '.');
+                case Keys.OemOpenBrackets: return (shift ? '{' : '[');
+                case Keys.OemQuotes: return (shift ? '"' : '\'');
+                case Keys.Oem1: return (shift ? ':' : ';');
+                case Keys.Oem3: return (shift ? '~' : '`');
+                case Keys.Oem5: return (shift ? '|' : '\\');
+                case Keys.Oem6: return (shift ? '}' : ']');
+                case Keys.Tab: return '\t';
+                case Keys.Space: return ' ';
+
+                // Number Pad
+                case Keys.NumPad0: return '0';
+                case Keys.NumPad1: return '1';
+                case Keys.NumPad2: return '2';
+                case Keys.NumPad3: return '3';
+                case Keys.NumPad4: return '4';
+                case Keys.NumPad5: return '5';
+                case Keys.NumPad6: return '6';
+                case Keys.NumPad7: return '7';
+                case Keys.NumPad8: return '8';
+                case Keys.NumPad9: return '9';
+                case Keys.Subtract: return '-';
+                case Keys.Decimal: return '.';
+                case Keys.Divide: return '/';
+                case Keys.Multiply: return '*';
+
+                default: return '\x00';
+            }
         }
     }
 }
